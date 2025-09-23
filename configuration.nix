f00e78115bfe5a32c079fd0ed6deb2598a269772
Nihal Kumarswamy <kumarnihal04@gmail.com>
2025-09-23 01:09:44 -0500

    initial

---
+++ configuration.nix
@@ -0,0 +1,82 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/Chicago";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.nihal = {
+    isNormalUser = true;
+    description = "nihal";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+openssh.authorizedKeys.keys = [
+"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILf2Rk2zBDt59KMamn3hFdyQwSu+hLy3yqwotpqZ5sDa nixos"
+];
+  };
+
+  # List packages installed in system profile. To search, run:
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+  #  wget
+  ];
+virtualisation.docker = {
+enable = true;
+rootless = {
+enable = true;
+setSocketVariable = true;
+};
+};
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Open ports in the firewall.
+   networking.firewall.allowedTCPPorts = [ 3030 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = true;
+
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
