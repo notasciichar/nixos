54dc1c5d7231788f408161a211329a315a0a3060
Nihal Kumarswamy <kumarnihal04@gmail.com>
2025-10-19 22:53:50 -0500

    styling

--- flake.nix
+++ flake.nix
@@ -6,13 +6,11 @@
   };
 
   outputs = { self, nixpkgs }: {
-
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-system = "x86_64-linux";
+      system = "x86_64-linux";
       modules = [
-./configuration.nix
-];
-
+        ./configuration.nix
+      ];
+    };
   };
-};
 }
